85e35d3887758a92292e2d323caeef35794d6526
David Sheffield <davesheff@live.co.uk>
2018-05-02 21:06:27 +0100

    location-details component added

---
+++ __tests__/components/location-details.test.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import LocationDetails from '../../src/components/location-details';
+
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
+import { location } from './data/forecast.json';
 
-render(<App name="David" />, document.getElementById('root'));
+render(<App location={location} />, document.getElementById('root'));
 
